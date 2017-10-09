a926d8f5a695c1fec074911acd43bdd51bad77d5
Idan Entin <idane@dudamobile.com>
2017-10-09 09:43:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Run Mocha tests on browsers
+
+A simple setup to run tests in your browser
+
+## How to use
+* Build using `npm run build`
+* Include the build result in the `index.html`
+* Include the test files in `index.html`
+* Open the file in your browser
---
+++ tests/hello.test.js
@@ -0,0 +1,10 @@
+describe('hello', () => {
+    it('should say hello to the world', () => {
+        expect(hello()).to.equal('Hello World!');
+    });
+
+    it('should say hello to a name', () => {
+        const name = 'John';
+        expect(hello(name)).to.equal(`Hello ${name}!`);
+    });
+});
